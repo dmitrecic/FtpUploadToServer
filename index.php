059cd3b2867874781b79abf8459be8a349ff69b3
dmitrecic <43320986+dmitrecic@users.noreply.github.com>
2018-09-17 19:16:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <?php
+
+// How to use FtpUploadToServer class
+// Example as follows
+// you can loop if there is more then one website where you need to upload your file
+// include class file
 require("FtpUploadToServer.class.php");
 
+// initialize class (format: "ftp server name", "ftp username", "ftp password", "ftp port")
+
 $ftp=new FtpUploadToServer("example.com","username","password","21");
+
+// show messages while trying to connect to FTP? (1=yes, 0=no, default:1)
 $ftp->setShowMessages(1);
+
+// show status when upload is finished? (status: succes or fail, 1=yes, 0=no, default:1)
 $ftp->setShowStatus(1);
+
+// create folder if destination folder not exists? (1=yes, 0=no, default:0)
 $ftp->setCreateFolder(1);
+
+// overwrite destination file if exist? (1=yes, 0=no, default:1)
 $ftp->setFileOverwrite(1);
+
+// set destination folder (default:/ - root)
 $ftp->setFolder("testclass/insidefolder");
+
+// set file to upload
 $ftp->setFile("test.txt");
+
+// finally - upload :)
 $ftp->ftpUpload();
 
 ?>
