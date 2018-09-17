9f426ada29671d7ace7b95b8c2ec1dde25f72a3c
dmitrecic <43320986+dmitrecic@users.noreply.github.com>
2018-09-17 19:34:56 +0200

    Add files via upload

---
+++ several-websites.php
@@ -0,0 +1,33 @@
+<?php
+require("classes/FtpUploadToServer.class.php");
+
+//
+// insert all websites, where you would like to connect and upload your file, into array
+// with FTP details as follows:
+//
+
+$websites=array(
+        array("ftp.examplesite-1.com", "ftpusername", "ftppassword","21"),
+        array("ftp.examplesite-2.com", "ftpusername", "ftppassword","21"),
+        array("ftp.examplesite-3.com", "ftpusername", "ftppassword","21"),
+);
+
+
+//
+// now loop array and upload file to each of those websites 
+//
+
+foreach($websites as $website=>$ftpdata){
+
+    $ftp=new FtpUploadToServer($ftpdata[0],$ftpdata[1],$ftpdata[2],$ftpdata[3]);
+    $ftp->setShowMessages(1);
+    $ftp->setShowStatus(1);
+    $ftp->setCreateFolder(1);
+    $ftp->setFileOverwrite(1);
+    $ftp->setFolder("testclass/insidefolder");
+    $ftp->setFile("test.txt");
+    $ftp->ftpUpload();
+
+}
+
+?>
